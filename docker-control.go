a98395baa1fb05030a7597e6999427bcc0b8c832
Simon Levesque <simon@foilen.com>
2019-01-05 09:28:29 -0500

    #2 Add "start" command

--- docker-control.go
+++ docker-control.go
@@ -5,6 +5,13 @@ import (
 	"syscall"
 )
 
+func showStart(containerName string) {
+	err := syscall.Exec("/usr/bin/docker", []string{"docker", "start", containerName}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func showStop(containerName string) {
 	err := syscall.Exec("/usr/bin/docker", []string{"docker", "stop", containerName}, nil)
 	if err != nil {
